d9876fa007780dc1bd031a2a96f4273a7ee3a208
ManuelSolano15 <2009127@upy.edu.mx>
2022-02-25 22:44:53 -0600

    two

--- Unit_2/Activity n.c
+++ Unit_2/Activity n.c
@@ -1,8 +1,24 @@
+//Create an algorithm to determine if a number is prime or not.
 #include <stdio.h>
 
 int main(){
-    
-    printf("Hola mundo");
-    
+    int i, num, cont=0;
+    printf ("put a number: ");
+    scanf ("%i", num);
+
+    for(i=1; i<=num; i++){
+        if(num%i==0){
+            cont++;
+        }
+    }
+
+    if(cont<2){
+        printf("The number is not prime");
+    }
+    else{
+        printf("The number prime");
+    }
+
+
     return 0;
 }
