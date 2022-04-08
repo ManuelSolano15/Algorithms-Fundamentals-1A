932addca0e9ccea24c787d0bc3a1348e1f86def1
ManuelSolano15 <2009127@upy.edu.mx>
2022-04-07 19:52:20 -0500

    one

---
+++ Unit_1/bucles_even_odd.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int cont = 100;
+    printf("hasta que numero vas a llegar:\n");
+    scanf("%d",&cont);
+
+    printf("Numeros pares: \n");
+    for (int i = 0; i <= cont; i++){
+        if (i % 2 == 0){
+            printf("%d\n", i);
+        }
+    }
+    
+    printf("Números impares: \n");
+    int ind=0;
+    while(ind != cont){
+        if(ind % 2 != 0){
+            printf("%d\n", ind);
+        }
+        ind++;
+    }
+    
+    return 0;
+}
