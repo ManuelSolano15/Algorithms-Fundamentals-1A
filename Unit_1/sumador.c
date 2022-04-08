9e8dff0bfcc28eb6ae58b88c47eedb8d121dbaee
ManuelSolano15 <2009127@upy.edu.mx>
2022-04-07 19:20:28 -0500

    one

---
+++ Unit_1/sumador.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+
+int num1,num2;
+
+int main(){
+    printf("Ingresa numero1");
+    scanf("%d", &num1);
+    printf("Ingresa numero2");
+    scanf("%d", &num2);
+    int sum= num1 + num2;
+
+    printf("La suma es: %d\n", sum);
+    return 0;
+}
